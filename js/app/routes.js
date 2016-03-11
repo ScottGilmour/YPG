ca8d4c21bd84768a88284ccad682c138410ea5ad
Scott Gilmour <scottryangilmour@gmail.com>
2016-03-11 12:50:49 -0500

    kill me

--- js/app/routes.js
+++ js/app/routes.js
@@ -203,7 +203,7 @@ module.exports = function(app, passport) {
                 res.sendStatus(403);
             }
 
-            if (!user) {
+            if (!user_obj) {
                 console.log('No user found');
                 res.sendStatus(403);
             } else {
