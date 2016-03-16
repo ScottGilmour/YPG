d6aa265ef4da4771753554bd9a0bcafa63ac25d1
Scott Gilmour <scottryangilmour@gmail.com>
2016-03-16 09:40:16 -0400

    kill me

--- js/app/routes.js
+++ js/app/routes.js
@@ -395,7 +395,7 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/', // redirect to the secure profile section
+        successRedirect : '/scraper', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
--- js/views/js/settings.js
+++ js/views/js/settings.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-
 	$('#localeDropdown').dropdown();
 
 	$('.menu .item').tab();
+
+	$('#unsubscribe').click(function(ev) {
+		$.ajax({
+			url: '/delete_subscription',
+			type: 'post'
+		})
+		.done(function() {
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	});
 });
