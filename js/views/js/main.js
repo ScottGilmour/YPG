9792630b3d3a6731858f3c0945a9dc3b1cef6d77
Scott Gilmour <scottryangilmour@gmail.com>
2016-03-23 15:07:53 -0400

    coldplay

--- js/views/js/main.js
+++ js/views/js/main.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
 	var field_count = 0;
 	var current_page = 0;
 
-
-
 	if(window.location.href.indexOf("https") > -1) {
        window.location.replace("http://104.196.23.57/scraper");
     }
